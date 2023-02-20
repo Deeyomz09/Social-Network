c1c699f447ca656d440f5a76560c2bf0c501cc7b
Deeyomz <qdjfco@tip.edu.ph>
2023-02-21 01:10:18 +0800

    Added request & body validation

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const userApi = require("./routes/api/users");
+const authApi = require("./routes/api/auth");
 
 const app = express();
 
 //Database Connection
 connectDB();
 
+// Init Middlware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Api Running"));
+
+//routes
+app.use("/api/users", userApi);
+app.use("/api/auth", authApi);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-const user = { name: "Diomedes", age: 27, isPrgramming: true };
