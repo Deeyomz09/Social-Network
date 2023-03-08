c0f43e73fc595e62aac11e0c8115dd3a50092c41
Deeyomz <qdjfco@tip.edu.ph>
2023-03-09 00:37:55 +0800

    Added Landing Page/User Interface

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const User = require("../../models/User");
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 //@route POST api/users
 // @desc Register user
 // @access public
 
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Name is required").not().isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or more characters"
+      'password',
+      'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
@@ -35,14 +36,14 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "User Already Exists" }] });
+          .json({ errors: [{ msg: 'User Already Exists' }] });
       }
 
       // Get users gravatar
       const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
+        s: '200',
+        r: 'pg',
+        d: 'mm',
       });
 
       user = new User({
@@ -66,7 +67,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        config.get('jwtSecret'),
         { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
@@ -74,7 +75,7 @@ router.post(
         }
       );
     } catch (err) {
-      res.status(500).send("Server error");
+      res.status(500).send('Server error');
     }
   }
 );
